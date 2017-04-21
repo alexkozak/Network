aaa985d449735d8bf4dc8c687fdb7e2f7612a20f
Александр <mr.galaktika@gmail.com>
2017-04-21 11:42:49 +0700

    home

--- src/Test/HomeHello.java
+++ src/Test/HomeHello.java
@@ -4,6 +4,6 @@ package Test;
 public class HomeHello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from home");
+        System.out.println("Hello from home 2");
     }
 }
